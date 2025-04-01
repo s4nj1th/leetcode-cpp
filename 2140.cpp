7fc6c63c31ce1280b400c7158afe23023770debc
s4nj1th <sanjith@tutamail.com>
2025-04-01 18:01:51 +0530

    daily problem 01-04-2025

---
+++ 2140.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  long long mostPoints(vector<vector<int>> &questions) {
+    int n = questions.size();
+    vector<long long> dp(n + 1, 0);
+
+    for (int i = n - 1; i >= 0; i--) {
+      int points = questions[i][0];
+      int jump = questions[i][1];
+
+      long long solve = points + (i + jump + 1 < n ? dp[i + jump + 1] : 0);
+
+      long long skip = dp[i + 1];
+
+      dp[i] = max(solve, skip);
+    }
+
+    return dp[0];
+  }
+};
