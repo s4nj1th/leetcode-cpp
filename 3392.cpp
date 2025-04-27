48b8b56e9d079e50cd0ec9f9b6fd25c949f63481
s4nj1th <sanjith@tutamail.com>
2025-04-27 16:59:10 +0530

    daily problem 27-04-2025

---
+++ 3392.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int countSubarrays(vector<int> &nums) {
+    int n = nums.size();
+    int count = 0;
+    for (int i = 0; i < n - 2; i++) {
+      if ((float)(nums[i] + nums[i + 2]) == (float)nums[i + 1] / 2)
+        count++;
+    }
+
+    return count;
+  }
+};
