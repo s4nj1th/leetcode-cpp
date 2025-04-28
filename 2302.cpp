fb4125db1dd4ffec3e693978fe0fd16714f01c2b
s4nj1th <sanjith@tutamail.com>
2025-04-28 14:39:29 +0530

    daily problem 28-04-2025

---
+++ 2302.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  long long countSubarrays(vector<int> &nums, long long k) {
+    int n = nums.size();
+    long long res = 0;
+    long long total = 0;
+
+    for (int i = 0, j = 0; j < n; j++) {
+
+      total += nums[j];
+
+      while (i <= j && total * (j - i + 1) >= k) {
+        total -= nums[i];
+        i++;
+      }
+
+      res += j - i + 1;
+    }
+
+    return res;
+  }
+};
