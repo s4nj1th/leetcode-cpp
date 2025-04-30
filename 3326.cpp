def2bffd4431afe421132446fcff702753c59095
s4nj1th <sanjith@tutamail.com>
2025-04-30 15:40:38 +0530

    random qn

---
+++ 3326.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  int minOperations(vector<int> &nums) {
+    int res = 0;
+    for (int i = nums.size() - 1; i > 0; --i) {
+      if (nums[i] < nums[i - 1]) {
+        nums[i - 1] = findNum(nums[i], nums[i - 1]);
+        if (nums[i - 1] == -1)
+          return -1;
+        res++;
+      }
+    }
+    return res;
+  }
+
+  int findNum(int n1, int n2) {
+    for (int i = 2; i < n1 + 1; ++i) {
+      if (n2 % i == 0)
+        return i;
+    }
+    return -1;
+  }
+};
