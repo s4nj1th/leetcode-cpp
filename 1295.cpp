a097d94091a6a1527baa1dd1506a769a19473dfa
s4nj1th <sanjith@tutamail.com>
2025-04-30 12:41:58 +0530

    daily problem 30-04-2025

---
+++ 1295.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int findNumbers(vector<int> &nums) {
+    int count = 0;
+
+    for (int i : nums) {
+      if ((i >= 10 && i <= 99) || (i >= 1000 && i <= 9999) || i == 100000)
+        count++;
+    }
+
+    return count;
+  }
+};
