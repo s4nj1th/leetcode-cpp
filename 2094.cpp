a34bfc7849eb0b21cf7de43add7cf9f3520400e9
s4nj1th <sanjith@tutamail.com>
2025-05-13 00:14:39 +0530

    daily problem 13-05-2025

---
+++ 2094.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  vector<int> findEvenNumbers(vector<int> &digits) {
+    vector<int> count(10);
+    for (int i : digits)
+      count[i]++;
+
+    vector<int> res;
+    for (int i = 1; i < 10; ++i) {
+      if (count[i] == 0)
+        continue;
+      count[i]--;
+      for (int j = 0; j < 10; ++j) {
+        if (count[j] == 0)
+          continue;
+        count[j]--;
+        for (int k = 0; k < 10; k += 2) {
+          if (count[k] == 0)
+            continue;
+          res.push_back(i * 100 + j * 10 + k);
+        }
+        count[j]++;
+      }
+      count[i]++;
+    }
+    return res;
+  }
+};
