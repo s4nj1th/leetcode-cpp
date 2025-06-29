1e4b8ebfd73784d1acb567bdf9b4d48402b6acff
s4nj1th <sanjith@tutamail.com>
2025-06-29 19:03:44 +0530

    daily problem 29-06-2025

---
+++ 1498.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  int numSubseq(vector<int> &nums, int target) {
+    int mod = 1e9 + 7;
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    vector<int> power(n, 1);
+    for (int i = 1; i < n; ++i) {
+      power[i] = (power[i - 1] * 2) % mod;
+    }
+
+    int l = 0;
+    int r = n - 1;
+    int ans = 0;
+
+    while (l <= r) {
+      if (nums[l] + nums[r] <= target) {
+        ans = (ans + power[r - l]) % mod;
+        l++;
+      } else {
+        r--;
+      }
+    }
+    return ans;
+  }
+};
