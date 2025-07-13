09f9c4bd8ce6db31e429967a406b6695af1aa9c6
s4nj1th <sanjith@tutamail.com>
2025-07-13 13:15:32 +0530

    similar to daily problem (13-07-2025)

--- 0455.cpp
+++ 0455.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-  int findContentChildren(vector<int> &g, vector<int> &s) {
-    int n = g.size();
-    int m = s.size();
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
-    sort(g.begin(), g.end());
-    sort(s.begin(), s.end());
+        int i = 0;
+        int j = 0;
+        int count = 0;
 
-    int i = 0;
-    int j = 0;
-    int count = 0;
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
 
-    while (i < n && j < m) {
-      if (s[i] >= g[j]) {
-        count++;
-        i++;
-        j++;
-      } else {
-        j++;
-      }
+        return count;
     }
-
-    return count;
-  }
 };
