f95ed56ae6b450281be54410ce324f7ac1c8a994
s4nj1th <sanjith@tutamail.com>
2025-08-05 19:30:47 +0530

    daily problem 05-07-2025

--- 3477.cpp
+++ 3477.cpp
@@ -19,3 +19,25 @@ public:
     return missing;
   }
 };
+
+// better soln
+class Solution {
+public:
+  int numOfUnplacedFruits(vector<int> &fruits, vector<int> &baskets) {
+    int n = fruits.size();
+
+    int missing = 0;
+
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (fruits[i] <= baskets[j]) {
+          baskets[j] = 0;
+          break;
+        } else if (j == (n - 1)) {
+          ++missing;
+        }
+      }
+    }
+    return missing;
+  }
+};
