0dd436a99cffebcf138e9698bfd3912961acf101
s4nj1th <sanjith@tutamail.com>
2025-08-20 15:11:38 +0530

    daily problem 20-08-2025

---
+++ 1277.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  int countSquares(vector<vector<int>> &matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    int ans = 0;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (matrix[i][j]) {
+          dp[i + 1][j + 1] = min({dp[i][j], dp[i + 1][j], dp[i][j + 1]}) + 1;
+          ans += dp[i + 1][j + 1];
+        }
+      }
+    }
+
+    return ans;
+  }
+};
