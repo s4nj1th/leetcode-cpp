969bf34d63b8c1a5b22204e58d812e1d98c69c10
sanjith <sanjith@tutamail.com>
2026-04-28 20:24:45 +0530

    daily question 28-04-2026

--- 2033.cpp
+++ 2033.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-  int minOperations(vector<vector<int>> &grid, int x) {
-    int m = grid.size();
-    int n = grid[0].size();
-    int rem = grid[0][0] % x;
-    vector<int> arr;
-    for (vector<int> i : grid) {
-      for (int j : i) {
-        if (j % x != rem)
-          return -1;
-        arr.push_back(j);
-      }
-    }
-    sort(arr.begin(), arr.end());
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> arr;
+        int ops = 0;
 
-    int ops = 0;
-    int finCom = arr[m * n / 2];
-    for (int i : arr) {
-      ops += abs(finCom - i) / x;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int rem = grid[0][0] % x;
+
+        for (vector<int> i : grid) {
+            for (int j : i) {
+                arr.push_back(j);
+                if (rem != (j % x)) return -1;
+            }
+        }
+
+        sort(arr.begin(), arr.end());
+
+        int finCom = arr[m*n/2];
+        for (int i : arr) {
+            ops += abs(finCom - i) / x;
+        }
+
+        return ops;
     }
-    return ops;
-  }
 };
